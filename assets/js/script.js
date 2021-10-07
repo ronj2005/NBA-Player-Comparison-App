aff5299af9e98f59dfea63819cb133e0f64dc647
Aaron Jones <ronj2005@gmail.com>
2021-10-07 14:25:40 -0400

    HTML update

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+var team1Name = $("");
+var team2Name = $("");
+var team1ID = $("");
+var team2ID = $("");
+var team1Icon = $("");
+var team2Icon = $("");
+
+
+function curTime (){
+    var today = moment().format("MMM DD, YYYY");
+    console.log(today);
+}
+
+
+function showCurGames () {
+
+    var 
+}
+
